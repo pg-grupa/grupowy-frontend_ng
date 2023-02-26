df1a50786db82b5a17d66e5b0d5e67dbbf04ae34
Konrad M <10982159+konmacie@users.noreply.github.com>
2023-02-26 14:56:26 +0100

    add api post request and error handling

--- src/app/pages/report-page/general-report/general-report.component.ts
+++ src/app/pages/report-page/general-report/general-report.component.ts
@@ -1,5 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { catchError } from 'rxjs';
+import { APIService } from 'src/app/core/services/api.service';
 
 // TODO: Add link to this component (in 'About' page?)
 @Component({
@@ -9,22 +12,47 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class GeneralReportComponent {
   maxLength: number = 512;
   length: number = 0;
+  success: boolean = false;
+  errorMessages: string[] = [];
 
   form: FormGroup = this._fb.group({
     message: this._fb.control('', [Validators.required]),
   });
 
-  constructor(private _fb: FormBuilder) {}
+  public get fc() {
+    return this.form.controls;
+  }
+
+  constructor(private _fb: FormBuilder, private _apiService: APIService) {}
 
   countLength() {
     this.length = this.form.controls['message'].value.length;
   }
 
   submit() {
-    console.log(this.form.value);
+    this._apiService
+      .postReport(this.form.value)
+      .pipe(
+        catchError((httpError) => {
+          if (httpError instanceof HttpErrorResponse) {
+            if (typeof httpError.error === 'string') {
+              this.errorMessages = [httpError.error];
+            } else {
+              this.errorMessages = [];
+              for (const key in httpError.error) {
+                this.form.controls[key]?.setErrors({
+                  server: httpError.error[key],
+                });
+              }
 
-    throw new Error('Method not implemented.');
-
-    // TODO
+              if ('NON_FIELD_ERRORS' in httpError.error) {
+                this.errorMessages = httpError.error.NON_FIELD_ERRORS;
+              }
+            }
+          }
+          throw httpError;
+        })
+      )
+      .subscribe(() => (this.success = true));
   }
 }
--- src/app/pages/report-page/location-report/location-report.component.ts
+++ src/app/pages/report-page/location-report/location-report.component.ts
@@ -1,7 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { catchError, Observable, of } from 'rxjs';
 import { ILocationFull } from 'src/app/core/models/location';
+import { APIService } from 'src/app/core/services/api.service';
 
 @Component({
   templateUrl: './location-report.component.html',
@@ -10,15 +13,25 @@ import { ILocationFull } from 'src/app/core/models/location';
 export class LocationReportComponent implements OnInit {
   maxLength: number = 512;
   length: number = 0;
+  success: boolean = false;
+  errorMessages: string[] = [];
 
   form: FormGroup = this._fb.group({
     location: this._fb.control(null, [Validators.required]),
     message: this._fb.control('', [Validators.required]),
   });
 
+  public get fc() {
+    return this.form.controls;
+  }
+
   location!: ILocationFull;
 
-  constructor(private _route: ActivatedRoute, private _fb: FormBuilder) {}
+  constructor(
+    private _route: ActivatedRoute,
+    private _fb: FormBuilder,
+    private _apiService: APIService
+  ) {}
 
   ngOnInit(): void {
     this._route.data.subscribe((data) => {
@@ -34,10 +47,29 @@ export class LocationReportComponent implements OnInit {
   }
 
   submit() {
-    console.log(this.form.value);
+    this._apiService
+      .postReport(this.form.value)
+      .pipe(
+        catchError((httpError) => {
+          if (httpError instanceof HttpErrorResponse) {
+            if (typeof httpError.error === 'string') {
+              this.errorMessages = [httpError.error];
+            } else {
+              this.errorMessages = [];
+              for (const key in httpError.error) {
+                this.form.controls[key]?.setErrors({
+                  server: httpError.error[key],
+                });
+              }
 
-    throw new Error('Method not implemented.');
-
-    // TODO
+              if ('NON_FIELD_ERRORS' in httpError.error) {
+                this.errorMessages = httpError.error.NON_FIELD_ERRORS;
+              }
+            }
+          }
+          throw httpError;
+        })
+      )
+      .subscribe(() => (this.success = true));
   }
 }
